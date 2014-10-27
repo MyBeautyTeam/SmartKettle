47cc299782d7b7d49dd8bf31cb2f883386b1c5d9
Jane Sycheva <e.sycheva_nn@mail.ru>
2014-10-28 01:54:58 +0300

    Adding_task in process

---
+++ app/src/main/java/com/beautyteam/smartkettle/Fragments/TasksFragment.java
@@ -0,0 +1,26 @@
+package com.beautyteam.smartkettle.Fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.beautyteam.smartkettle.Fragments.Adapter.NewsListAdapter;
+import com.beautyteam.smartkettle.MainActivity;
+import com.beautyteam.smartkettle.Mechanics.News;
+import com.beautyteam.smartkettle.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 173042 on 28.10.2014.
+ */
+public class TasksFragment extends Fragment {
+}
