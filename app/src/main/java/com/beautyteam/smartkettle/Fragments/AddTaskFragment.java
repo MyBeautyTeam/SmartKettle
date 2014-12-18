9357bca8c36ea6cd3dd40df2c306e9c09ffa9e2e
Jane Sycheva <e.sycheva_nn@mail.ru>
2014-12-18 15:31:41 +0300

    add spinner;

--- app/src/main/java/com/beautyteam/smartkettle/Fragments/AddTaskFragment.java
+++ app/src/main/java/com/beautyteam/smartkettle/Fragments/AddTaskFragment.java
@@ -1,6 +1,7 @@
 package com.beautyteam.smartkettle.Fragments;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,10 +11,13 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
 
+import com.beautyteam.smartkettle.Database.DevicesContract;
+import com.beautyteam.smartkettle.Database.SmartContentProvider;
 import com.beautyteam.smartkettle.MainActivity;
 import com.beautyteam.smartkettle.R;
 
@@ -31,6 +35,9 @@ public class AddTaskFragment extends Fragment implements  View.OnClickListener{
     private String spinnerSelected;
     private MainActivity mCallback;
     private Fragment self;
+
+    // Проверка на существование
+    private static final String[] DEVICE_PROJECTION = { DevicesContract.DevicesEntry.COLUMN_NAME_TITLE };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,6 +74,23 @@ public class AddTaskFragment extends Fragment implements  View.OnClickListener{
 
             }
         });
+
+        Cursor cursor = getActivity().getContentResolver().query(
+                SmartContentProvider.DEVICE_CONTENT_URI,
+                DEVICE_PROJECTION,
+                null,
+                null,
+                null
+        );
+        if (cursor.getCount() > 0){
+            String[] from = new String[]{ DevicesContract.DevicesEntry.COLUMN_NAME_TITLE };
+            int[] to = new int[]{ android.R.id.text1 };
+            SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_spinner_item,
+                    cursor, from, to);
+            mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(mAdapter);
+        }
+
     }
 
     @Override
@@ -81,6 +105,7 @@ public class AddTaskFragment extends Fragment implements  View.OnClickListener{
             case R.id.addTaskOkBtn:
                 DatePicker datePicker = (DatePicker) linearLayout.findViewById(R.id.addTaskDate);
                 TimePicker timePicker = (TimePicker) linearLayout.findViewById(R.id.addTaskTime);
+                timePicker.setIs24HourView(true);
                 Date date = new Date(datePicker.getYear() - 1900, datePicker.getMonth(), datePicker.getDayOfMonth(),
                         timePicker.getCurrentHour(), timePicker.getCurrentMinute());
                 mCallback.addTask(date, spinnerSelected);
